19df2cfcd50210f86ba7d6a15a0815dcc6626b01
wangzelin <wangzelin@meituan.com>
2019-03-29 22:04:45 +0800

    to c

--- wxApp/src/pages/history/index.vue
+++ wxApp/src/pages/history/index.vue
@@ -1,12 +1,113 @@
 <template>
-  <div class="mainWrap">
+  <div>
+
+    <div style="width: 100vw;height: 100px;">
+      <div style="float: left;width: 33%;height: 100%;text-align: center;line-height: 100%;">
+        <img src="https://2aclgika1.i-plc.cn/static/images/biek.png" style="margin: 0 auto;display: unset;">
+      </div>
+      <div style="float: left;width: 67%;height: 100%;text-align: center;display: flex;display: -webkit-flex;justify-content:center;align-items: center;">
+        <div>
+          <p style="font-size: 16px;font-weight: bold">人人车</p>
+          <p style="font-size: 13px;color: #999999">2018年交易量：1000辆</p>
+          <p style="font-size: 13px;color: #999999">月交易额：100辆</p>
+        </div>
+
+      </div>
+    </div>
+
+    <div  class="canvas">
+      <ec-canvas id="mychart1" canvas-id="mychart-bar1" class="canvas" :ec="sale"></ec-canvas>
+    </div>
+    <div  class="canvas">
+      <ec-canvas id="mychart2" canvas-id="mychart-bar2" class="canvas" :ec="profit"></ec-canvas>
+    </div>
+
+
+
+
   </div>
 </template>
 
 <script>
+  const saleOptions = {
+    title: {
+      text: '销售情况'
+    },
+    tooltip: {
+      trigger: 'axis',
+      position: 'bottom',
+      formatter: "{b} : {c}千辆"
+    },
+    xAxis:  {
+      type: 'category',
+      data: ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        name:'盈利情况',
+        type:'line',
+        data:[12, 34, 5, 61, 7, 12, 32,1,2,3,4,12],
+        markPoint: {
+          data: [
+            {type: 'max', name: '最大值'},
+            {type: 'min', name: '最小值'}
+          ]
+        },
+        markLine: {
+          data: [
+            {type: 'average', name: '平均值'}
+          ]
+        }
+      }
+    ]
+  };
+  const profitOptions = {
+    title: {
+      text: '盈利情况'
+    },
+    tooltip: {
+      trigger: 'axis',
+      position: 'bottom',
+      formatter: "{b} : {c}万元"
+    },
+    xAxis:  {
+      type: 'category',
+      data: ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月']
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        name:'盈利情况',
+        type:'line',
+        data:[1000, 800, 1111, 2224, 555, 1661, 880,144,1442,123,5151,151],
+        markPoint: {
+          data: [
+            {type: 'max', name: '最大值'},
+            {type: 'min', name: '最小值'}
+          ]
+        },
+        markLine: {
+          data: [
+            {type: 'average', name: '平均值'}
+          ]
+        }
+      }
+    ]
+  };
 export default {
   data() {
     return {
+      sale: {
+        options: saleOptions
+      },
+      profit:{
+        options: profitOptions
+      }
     };
   },
   methods: {},
@@ -19,9 +120,9 @@ export default {
 </script>
 
 <style scoped>
-.mainWrap {
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-}
+  .canvas {
+    width: 100vw;
+    height: 400px;
+    padding: 10px;
+  }
 </style>
--- wxApp/src/pages/report/index.vue
+++ wxApp/src/pages/report/index.vue
@@ -11,65 +11,38 @@
 
 <script>
   const options = {
-    backgroundColor: '#2c343c',
-
     title: {
       text: '昨日各平台交易量',
-      left: 'center',
-      top: 20,
-      textStyle: {
-        color: '#ccc'
-      }
+      x:'center'
     },
 
     tooltip : {
       trigger: 'item',
       position: 'bottom',
-      formatter: "{b} : {c} ({d}%)"
+      formatter: "{b} : {c}辆 ({d}%)"
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left',
+      data: ['人人车','优享车','懂车帝','瓜子二手车']
     },
 
-    visualMap: {
-      show: false,
-      min: 80,
-      max: 600,
-      inRange: {
-        colorLightness: [0, 1]
-      }
-    },
     series : [
       {
         name:'交易平台',
         type:'pie',
         radius : '55%',
-        center: ['50%', '50%'],
+        center: ['50%', '60%'],
         data:[
           {value:1000, name:'人人车'},
           {value:1200, name:'优享车'},
           {value:800, name:'懂车帝'},
           {value:1700, name:'瓜子二手车'}
-        ].sort(function (a, b) { return a.value - b.value; }),
-        roseType: 'radius',
-        label: {
-          normal: {
-            textStyle: {
-              color: 'rgba(255, 255, 255, 0.3)'
-            }
-          }
-        },
-        labelLine: {
-          normal: {
-            lineStyle: {
-              color: 'rgba(255, 255, 255, 0.3)'
-            },
-            smooth: 0.2,
-            length: 10,
-            length2: 20
-          }
-        },
+        ],
         itemStyle: {
-          normal: {
-            color: '#c23531',
-            shadowBlur: 200,
+          emphasis: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
             shadowColor: 'rgba(0, 0, 0, 0.5)'
           }
         }
@@ -81,7 +54,7 @@ export default {
     return {
       ec: {
         // 传 options
-        options: options,
+        options: options
       }
     };
   },
